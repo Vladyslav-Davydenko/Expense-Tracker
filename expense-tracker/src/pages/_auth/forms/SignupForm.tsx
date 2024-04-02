4869e88e71e8fd61c26117357deb511b0d6df453
Vladyslav <5vladik068@gmail.com>
2024-04-02 20:56:59 +0300

    Small sign up update (form)

--- expense-tracker/src/pages/_auth/forms/SignupForm.tsx
+++ expense-tracker/src/pages/_auth/forms/SignupForm.tsx
@@ -1,42 +1,63 @@
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
 import { Button } from "@/components/ui/button";
 
-import { useDebouncedCallback } from "@/hooks/useDebouncedCallback";
-import { useCallback, useEffect, useRef, useState } from "react";
+const formScheme = z.object({
+  username: z
+    .string()
+    .min(2, { message: "Username should be at least 2 symbols" })
+    .max(50, { message: "USername should not be more than 50 symbols" }),
+});
 
 export const SignupForm = () => {
-  const [counter, setCounter] = useState<number>(0);
-  // const [test, setTest] = useState<boolean>(false);
-  const firstRender = useRef(false);
+  const form = useForm<z.infer<typeof formScheme>>({
+    resolver: zodResolver(formScheme),
+    defaultValues: {
+      username: "",
+    },
+  });
 
-  // Cashing data to prevent from unnecessary re-renders
-  const fetchingData = useCallback(
-    useDebouncedCallback((data) => {
-      console.log(`Fetching: ${data}...`);
-    }),
-    [counter]
-  );
-
-  useEffect(() => {
-    if (firstRender.current) {
-      fetchingData(counter);
-    }
-    firstRender.current = true;
-  }, [counter, fetchingData]);
-
-  const handleClickButton = () => {
-    // setTest(!test);
-    setCounter((prev) => {
-      console.log(`Times rerendered: ${prev + 1}`);
-      return prev + 1;
-    });
+  const onSubmit = () => {
+    console.log("Pressed");
   };
 
   return (
     <div>
-      <Button className=" bg-primary-light" onClick={handleClickButton}>
-        Signup
-      </Button>
-      <p>{counter}</p>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </div>
   );
 };
