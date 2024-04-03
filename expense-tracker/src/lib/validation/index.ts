26a2be594a37ac2ef4cf74012abe9d027d963af9
Vladyslav <5vladik068@gmail.com>
2024-04-03 09:15:06 +0300

    Sign-up form finished

---
+++ expense-tracker/src/lib/validation/index.ts
@@ -0,0 +1,16 @@
+import * as z from "zod";
+
+export const SignupValidation = z.object({
+  name: z
+    .string()
+    .min(2, { message: "Name should be at least 2 symbols" })
+    .max(50, { message: "Name should not be more than 50 symbols" }),
+  username: z
+    .string()
+    .min(2, { message: "Username should be at least 2 symbols" })
+    .max(50, { message: "Username should not be more than 50 symbols" }),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Password should be at least 8 characters." }),
+});
--- expense-tracker/src/pages/_auth/forms/SignupForm.tsx
+++ expense-tracker/src/pages/_auth/forms/SignupForm.tsx
@@ -1,12 +1,14 @@
-import * as z from "zod";
+import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
+import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+
+import { SignupValidation } from "@/lib/validation";
 
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -15,47 +17,118 @@ import {
 import { Input } from "@/components/ui/input";
 
 import { Button } from "@/components/ui/button";
-
-const formScheme = z.object({
-  username: z
-    .string()
-    .min(2, { message: "Username should be at least 2 symbols" })
-    .max(50, { message: "USername should not be more than 50 symbols" }),
-});
+import Loader from "@/components/shared/Loader";
 
 export const SignupForm = () => {
-  const form = useForm<z.infer<typeof formScheme>>({
-    resolver: zodResolver(formScheme),
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
     defaultValues: {
+      name: "",
       username: "",
+      email: "",
+      password: "",
     },
   });
 
-  const onSubmit = () => {
-    console.log("Pressed");
+  // For now should be like this
+  const isLoading = false;
+
+  const onSubmit = (values: z.infer<typeof SignupValidation>) => {
+    //Create the user and send it to backend for JWT token
+    console.log(values);
   };
 
   return (
-    <div>
+    <div className="sm:w-[320px] flex justify-center items-center flex-col">
+      <h2 className=" text-lg font-bold md:text-xl pt-5 sm:pt-12">
+        Create a new account
+      </h2>
+      <p className=" text-primary-light text-sm">
+        To use Expense Tracker enter your details
+      </p>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className=" flex flex-col gap-5 w-full mt-4"
+        >
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="pl-2">Name</FormLabel>
+                <FormControl>
+                  <Input className="text-primary-dark" type="text" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <FormField
             control={form.control}
             name="username"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Username</FormLabel>
+                <FormLabel className="pl-2">Username</FormLabel>
                 <FormControl>
-                  <Input placeholder="shadcn" {...field} />
+                  <Input className="text-primary-dark" type="text" {...field} />
                 </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
           />
-          <Button type="submit">Submit</Button>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="pl-2">Email</FormLabel>
+                <FormControl>
+                  <Input
+                    className="text-primary-dark"
+                    type="email"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="pl-2">Password</FormLabel>
+                <FormControl>
+                  <Input
+                    className="text-primary-dark"
+                    type="password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="bg-primary-light">
+            {isLoading ? (
+              <div className="flex justify-center items-center gap-2">
+                <Loader />
+              </div>
+            ) : (
+              "Sign up"
+            )}
+          </Button>
+          <p className="text-sm text-white text-center mt-2">
+            Already have an account?
+            <Link
+              to="/sign-in"
+              className="text-primary-light font-semibold ml-1"
+            >
+              Log in
+            </Link>
+          </p>
         </form>
       </Form>
     </div>
