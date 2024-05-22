e86059bf9a2e4f57adbf0a0b1955190de9a97bda
Vladyslav <5vladik068@gmail.com>
2024-05-22 16:33:10 +0300

    Statistics section

--- expense-tracker/src/pages/_root/Statistics.tsx
+++ expense-tracker/src/pages/_root/Statistics.tsx
@@ -5,6 +5,8 @@ import {
   useGetTypes,
 } from "@/lib/react-query/QueriesAndMuntations";
 
+import { ArrowUp, ArrowDown } from "lucide-react";
+
 const Statistics = () => {
   const { data: types } = useGetTypes();
   const { data: expenses } = useGetExpenses();
@@ -14,7 +16,7 @@ const Statistics = () => {
       <h2 className=" text-xl font-semibold tracking-wider uppercase mb-20">
         Statistics
       </h2>
-      <div className="flex justify-center items-center gap-10">
+      <div className="grid grid-cols-2 gap-10 justify-center items-center mb-[150px]">
         <div className="rounded-md bg-primary shadow-md flex-1">
           <p className="p-4 rounded-md bg-primary shadow-md">
             Expenses statistics
@@ -26,6 +28,45 @@ const Statistics = () => {
             Income statistics
           </p>
           <BarChartExtended types={types || []} expenses={expenses || []} />
+        </div>
+        <div className="grid grid-cols-2 gap-10">
+          <div className="p-4 rounded-md bg-primary shadow-md">
+            <p className=" text-sm opacity-80">Largest expense</p>
+            <div className="mt-4 flex gap-1 justify-end items-center">
+              <p className="text-lg">$1234</p>
+              <ArrowDown className=" text-red-500" />
+              <span className="text-lg text-red-500 ">12%</span>
+            </div>
+          </div>
+          <div className="p-4 rounded-md bg-primary shadow-md">
+            <p className=" text-sm opacity-80">Biggest income</p>
+            <div className="mt-4 flex gap-1 justify-end items-center">
+              <p className="text-lg">$1234</p>
+              <ArrowUp className=" text-green-500" />
+              <span className="text-lg text-green-500 ">12%</span>
+            </div>
+          </div>
+          <div className="p-4 rounded-md bg-primary shadow-md">
+            <p className=" text-sm opacity-80">Comparison expenses</p>
+            <div className="mt-4 flex gap-1 justify-end items-center">
+              <p className="text-lg">$1234</p>
+              <ArrowDown className=" text-red-500" />
+              <span className="text-lg text-red-500 ">12%</span>
+            </div>
+          </div>
+          <div className="p-4 rounded-md bg-primary shadow-md">
+            <p className=" text-sm opacity-80">Comparison income</p>
+            <div className="mt-4 flex gap-1 justify-end items-center">
+              <p className="text-lg">$1234</p>
+              <ArrowUp className=" text-green-500" />
+              <span className="text-lg text-green-500 ">12%</span>
+            </div>
+          </div>
+        </div>
+        <div className="rounded-md bg-primary shadow-md flex-1">
+          <p className="p-4 rounded-md bg-primary shadow-md">
+            Income statistics
+          </p>
         </div>
       </div>
     </div>
