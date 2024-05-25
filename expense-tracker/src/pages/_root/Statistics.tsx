d1f74685f7d23deb2aa9a24dae9e8a6670109bf8
Vladyslav <5vladik068@gmail.com>
2024-05-25 13:16:42 +0300

    .

--- expense-tracker/src/pages/_root/Statistics.tsx
+++ expense-tracker/src/pages/_root/Statistics.tsx
@@ -32,34 +32,34 @@ const Statistics = () => {
         <div className="grid grid-cols-2 gap-10">
           <div className="p-4 rounded-md bg-primary shadow-md">
             <p className=" text-sm opacity-80">Largest expense</p>
-            <div className="mt-4 flex gap-1 justify-end items-center">
+            <div className="mt-4 flex justify-end items-end">
               <p className="text-lg">$1234</p>
               <ArrowDown className=" text-red-500" />
-              <span className="text-lg text-red-500 ">12%</span>
+              <span className="text-sm text-red-500 ">12%</span>
             </div>
           </div>
           <div className="p-4 rounded-md bg-primary shadow-md">
             <p className=" text-sm opacity-80">Biggest income</p>
-            <div className="mt-4 flex gap-1 justify-end items-center">
+            <div className="mt-4 flex justify-end items-start">
               <p className="text-lg">$1234</p>
               <ArrowUp className=" text-green-500" />
-              <span className="text-lg text-green-500 ">12%</span>
+              <span className="text-sm text-green-500 ">12%</span>
             </div>
           </div>
           <div className="p-4 rounded-md bg-primary shadow-md">
             <p className=" text-sm opacity-80">Comparison expenses</p>
-            <div className="mt-4 flex gap-1 justify-end items-center">
+            <div className="mt-4 flex justify-end items-end">
               <p className="text-lg">$1234</p>
               <ArrowDown className=" text-red-500" />
-              <span className="text-lg text-red-500 ">12%</span>
+              <span className="text-sm text-red-500 ">12%</span>
             </div>
           </div>
           <div className="p-4 rounded-md bg-primary shadow-md">
             <p className=" text-sm opacity-80">Comparison income</p>
-            <div className="mt-4 flex gap-1 justify-end items-center">
+            <div className="mt-4 flex justify-end items-start">
               <p className="text-lg">$1234</p>
               <ArrowUp className=" text-green-500" />
-              <span className="text-lg text-green-500 ">12%</span>
+              <span className="text-sm text-green-500 ">12%</span>
             </div>
           </div>
         </div>
